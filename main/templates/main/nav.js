afc91e0b189f2b5538b91a19aebf843b44429599
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-31 08:08:28 +0545

    navbar for smaller device, also responsive

---
+++ main/templates/main/nav.js
@@ -0,0 +1,7 @@
+const mentBtnEl = document.getElementsByClassName("menu-btn")[0];
+const menuEl = document.getElementsByClassName("menu")[0];
+mentBtnEl.addEventListener("click", () => {
+    menuEl.classList.toggle("menu-active");
+    console.log("clicked");
+
+});
