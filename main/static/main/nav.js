72e10cb80e95d385b1ef48c44fe20518c73ab437
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-31 08:17:36 +0545

    integration with django urls, templates, static files

--- main/static/main/nav.js
+++ main/static/main/nav.js
@@ -2,6 +2,5 @@ const mentBtnEl = document.getElementsByClassName("menu-btn")[0];
 const menuEl = document.getElementsByClassName("menu")[0];
 mentBtnEl.addEventListener("click", () => {
     menuEl.classList.toggle("menu-active");
-    console.log("clicked");
 
 });
